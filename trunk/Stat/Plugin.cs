e4067869da4f89d249112b5dd9860887064eee81
niels.rask <niels.rask@1b5b0610-1c28-0410-9348-adc7e293284f>
2007-04-26 04:41:35 +0000

    prototype stat module

--- trunk/Stat/Plugin.cs
+++ trunk/Stat/Plugin.cs
@@ -1,20 +1,29 @@
 using System;
 using NielsRask.FnordBot;
+using System.Collections;
+using log4net;
 
 namespace NielsRask.Stat
 {
 	public class StatPlugin : IPlugin
 	{
 		NielsRask.FnordBot.FnordBot bot;
+		ChannelDictionary wordstat;
+		ChannelDictionary userstat;
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
 		public StatPlugin()
 		{
+			wordstat = new ChannelDictionary();
+			userstat  = new ChannelDictionary();
 		}
 		#region IPlugin Members
 
 		public void Attach(NielsRask.FnordBot.FnordBot bot)
 		{
+			log.Info("Attaching stat plugin");
 			this.bot = bot;
+			bot.OnPublicMessage += new NielsRask.FnordBot.FnordBot.MessageHandler(bot_OnPublicMessage);
 		}
 
 		public void Init(System.Xml.XmlNode pluginNode)
@@ -24,5 +33,198 @@ namespace NielsRask.Stat
 
 		#endregion
 
+		private void bot_OnPublicMessage(User user, string channel, string message)
+		{
+			try 
+			{
+				if ( !message.StartsWith("!") ) 
+				{
+					#region wordstat
+					string[] parts = message.Split(' ');
+					foreach (string part in parts)
+						wordstat.Increment(channel, part); // bedre med en rigtig for-løkke
+					#endregion
+
+					#region userstat
+					userstat.Increment(channel, user.NickName); // tæl linjer. evt en wordcol også
+					#endregion
+				} 
+				else if (message == "!topwords") //eller toptalk{
+				{
+					log.Info("Listing top words on "+channel+"...");
+
+					StatCollection scol = wordstat[channel].GetTop(10);
+					for (int i=0; i<scol.Count; i++) 
+					{
+						bot.SendToChannel( channel, (i+1)+" "+scol[i].Key+": "+scol[i].Score, true);// forkert, skal bruge et dictionary eller en anden måde at få scores på. 
+					}
+				}
+				else if (message == "!toptalk") 
+				{
+					log.Info("Listing top talkers on "+channel+"...");
+
+					StatCollection scol = userstat[channel].GetTop(10);
+					for (int i=0; i<scol.Count; i++) 
+					{
+						bot.SendToChannel( channel, (i+1)+" "+scol[i].Key+": "+scol[i].Score, true);
+					}
+				}
+				else if (message=="!chanstat") 
+				{
+					log.Info("Listing stat info");
+					bot.SendToChannel(channel, wordstat[channel].Count+" words known in this channel");
+				}
+				else if (message=="!mystat") 
+				{
+					log.Info("Listing personal stats info");
+					bot.SendToChannel(channel, "Your score: "+userstat[channel][user.NickName].Score+"");
+
+				}
+			} 
+			catch (Exception e) 
+			{
+				log.Error("Error in statplugin.onpublicmessage('"+user.NickName+"','"+channel+"', '"+message+"')",e);
+			}
+		}
+	}
+
+	/// <summary>
+	/// et dictionary over kanalnavne og statdictionaries
+	/// </summary>
+	public class ChannelDictionary : DictionaryBase 
+	{
+		public void Increment(string channel, string word) 
+		{
+			if (Contains(channel))
+				Add(channel, new StatCollection());
+
+			this[channel].Increment(word);
+		}
+
+		public void Add(string channelname, StatCollection collection) 
+		{
+			Dictionary.Add(channelname, collection);
+		}
+
+		public StatCollection this[string channelname] 
+		{
+			get { return (StatCollection)Dictionary[channelname]; }
+			set { Dictionary[channelname] = value; }
+		}
+
+		public bool Contains(string key)
+		{
+			return this.Dictionary.Contains(key);
+		}
+
+		public ICollection Keys
+		{
+			get {return this.Dictionary.Keys;}
+		}
+
+	}
+
+	public class StatObject 
+	{
+		private string key;
+		private int score;
+
+		public string Key 
+		{
+			get { return key; }
+			set { key = value; }
+		}
+		public int Score 
+		{
+			get { return score; }
+			set { score = value; }
+		}
+
+		public StatObject(string key, int score) 
+		{
+			this.Key = key;
+			this.score = score;
+		}
+	}
+
+	public class StatCollection : CollectionBase 
+	{
+		public void Add(string key, int score) 
+		{
+			Add(new StatObject(key, score) );
+		}
+		public void Add(StatObject obj) 
+		{
+			List.Add( obj );
+		}
+
+		public void Increment(string key) 
+		{
+			StatObject obj= GetByKey(key);
+			if (obj == null) 
+			{
+				obj = new StatObject(key, 0);
+				Add( obj );
+			}
+			obj.Score++;
+		}
+
+		public StatCollection GetTop(int count) 
+		{
+			Sort();
+
+			StatCollection col = new StatCollection();
+			int max = count>10?10:count;
+
+			for(int i=0; i<max; i++)
+				col.Add( this[i] );
+
+			return col; 
+		}
+
+		private void Sort()	// bubblesort, bare for at få noget der virker. optimer senere!
+		{
+			int i, j;
+			StatObject temp;
+			for (i =Count - 1; i > 0; i--)
+			{
+				for (j = 0; j < i; j++)
+				{
+					if (this[j].Score.CompareTo(this[j + 1].Score) > 0)
+					{
+						temp = this[j];
+						this[j] = this[j+1];
+						this[j+1] = temp;
+					}
+				}
+			}
+		}
+		public StatObject this[int i] 
+		{
+			get { return (StatObject)List[i]; }
+			set { List[i] = value; }
+		}
+
+		public StatObject this[string key] 
+		{
+			get { return GetByKey(key); }
+		}
+
+		private StatObject GetByKey(string key) 
+		{
+			bool found = false;
+			int i = 0;
+			while (!found && i<Count) 
+			{
+				if (this[i].Key == key) 
+					found = true;
+				else
+					i++;
+			}
+			if (found)
+				return this[i];
+			else
+				return null;
+		}
 	}
 }
